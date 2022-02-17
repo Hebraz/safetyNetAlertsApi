12387e1376d7bb990c876b8d022d72fc3fb75773
Pierrick Levazeux <pierrick.levazeux@protonmail.com>
2022-02-17 12:00:55 +0100

    Initial commit Project build by spring initializr with following starters:  - Spring Web  - Spring Data JPA  - MySQL Driver  - Lombok

---
+++ src/main/java/com/levazeuxpierrick/safetyNetAlertsApi/SafetyNetAlertsApiApplication.java
@@ -0,0 +1,13 @@
+package com.levazeuxpierrick.safetyNetAlertsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyNetAlertsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyNetAlertsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/levazeuxpierrick/safetyNetAlertsApi/SafetyNetAlertsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levazeuxpierrick.safetyNetAlertsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetAlertsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
