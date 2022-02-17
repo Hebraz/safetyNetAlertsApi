175c7a0a44b93a4a50f540c51aee82e0ad22a04e
Pierrick Levazeux <pierrick.levazeux@protonmail.com>
2022-02-17 16:34:02 +0100

    Configuration of application  - application name  - log level  - database connexion

--- src/main/java/com/safetynet/alerts/api/SafetyNetAlertsApiApplication.java
+++ src/main/java/com/safetynet/alerts/api/SafetyNetAlertsApiApplication.java
@@ -1,4 +1,4 @@
-package com.levazeuxpierrick.safetyNetAlertsApi;
+package com.safetynet.alerts.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/safetynet/alerts/api/SafetyNetAlertsApiApplicationTests.java
+++ src/test/java/com/safetynet/alerts/api/SafetyNetAlertsApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.levazeuxpierrick.safetyNetAlertsApi;
+package com.safetynet.alerts.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
